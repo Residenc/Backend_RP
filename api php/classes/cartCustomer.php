e4e8e594af9dce165dee29ec0aecf1d033948a8c
Yorman Rojas <yormanxrojas@gmail.com>
2022-02-18 02:13:00 -0600

    cart beta

--- api php/classes/cartCustomer.php
+++ api php/classes/cartCustomer.php
@@ -22,5 +22,26 @@
 
         }
 
+        public static function getCartItemsCustomer($cust_id){
+            $db = new Connection();
+            $query = "SELECT cartitemscustomers.cust_id, cartitemscustomers.quantity,cartitemscustomers.product_id,products.product_name, products.price FROM `cartitemscustomers` inner join products on cartitemscustomers.product_id = products.product_id where cartitemscustomers.cust_id = $cust_id";
+            $result = $db->query($query);
+            $data = [];
+            if ($result->num_rows) {
+                while ( ($row = $result->fetch_assoc()) ) {
+                    $data[]=[
+                        'product_id' => $row['product_id'],
+                        'cust_id' => $row['cust_id'],
+                        'product_name' => $row['product_name'],
+                        'price' => $row['price'],
+                        'quantity' => $row['quantity'],
+                        'total' => $row['price']*$row['quantity']
+                    ];
+                }
+                return $data;
+            }
+            return $data;
+        }
+
 
     }
--- api php/classes/cartVendor.php
+++ api php/classes/cartVendor.php
@@ -22,5 +22,26 @@
 
         }
 
+        public static function getCartItemsVendor($vendor_id){
+            $db = new Connection();
+            $query = "SELECT cartitemsvendors.vendor_id, cartitemsvendors.quantity,cartitemsvendors.product_id,products.product_name, products.price FROM `cartitemsvendors` inner join products on cartitemsvendors.product_id = products.product_id where cartitemsvendors.vendor_id = $vendor_id";
+            $result = $db->query($query);
+            $data = [];
+            if ($result->num_rows) {
+                while ( ($row = $result->fetch_assoc()) ) {
+                    $data[]=[
+                        'product_id' => $row['product_id'],
+                        'vendor_id' => $row['vendor_id'],
+                        'product_name' => $row['product_name'],
+                        'price' => $row['price'],
+                        'quantity' => $row['quantity'],
+                        'total' => $row['price']*$row['quantity']
+                    ];
+                }
+                return $data;
+            }
+            return $data;
+        }
+
 
     }
