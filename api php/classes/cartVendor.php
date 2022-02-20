537583736f75d0f0d8c9bc4c0f6cd887cd2c06c3
Yorman Rojas <yormanxrojas@gmail.com>
2022-02-19 21:41:07 -0600

    cambios en el carrito para cantidades

--- api php/classes/cartVendor.php
+++ api php/classes/cartVendor.php
@@ -11,25 +11,36 @@
     class CartVendor{
 
         public static function insertCartItemVendor($vendor_id, $product_id, $quantity){
-
             $db = new Connection();
+            $q=("SELECT * FROM cartitemsvendors WHERE product_id=$product_id AND vendor_id=$vendor_id");
+            $result = $db->query($q);
+            if ($result->num_rows){
+                $updateQuantity = "UPDATE cartitemsvendors SET quantity=quantity+'".$quantity."' WHERE product_id=$product_id AND vendor_id=$vendor_id";
+                $db->query($updateQuantity);
+                if ($db->affected_rows)
+                {
+    
+                    return TRUE;
+                }
+                return FALSE;
+            }
             $query = "INSERT INTO `cartitemsvendors`(`vendor_id`, `product_id`, `quantity`) VALUES ('".$vendor_id."', '".$product_id."', '".$quantity."')";
             $db->query($query);
             if ($db->affected_rows) {
                 return TRUE;
             }
             return FALSE;
-
         }
 
         public static function getCartItemsVendor($vendor_id){
             $db = new Connection();
-            $query = "SELECT cartitemsvendors.vendor_id, cartitemsvendors.quantity,cartitemsvendors.product_id,products.product_name, products.price FROM `cartitemsvendors` inner join products on cartitemsvendors.product_id = products.product_id where cartitemsvendors.vendor_id = $vendor_id";
+            $query = "SELECT cartitemsvendors.cartitem_id,  cartitemsvendors.vendor_id, cartitemsvendors.quantity,cartitemsvendors.product_id,products.product_name, products.price FROM `cartitemsvendors` inner join products on cartitemsvendors.product_id = products.product_id where cartitemsvendors.vendor_id = $vendor_id";
             $result = $db->query($query);
             $data = [];
             if ($result->num_rows) {
                 while ( ($row = $result->fetch_assoc()) ) {
                     $data[]=[
+                        'cartitem_id' => $row['cartitem_id'],
                         'product_id' => $row['product_id'],
                         'vendor_id' => $row['vendor_id'],
                         'product_name' => $row['product_name'],
@@ -43,5 +54,32 @@
             return $data;
         }
 
+        public static function deleteCartItemVendor($cartitem_id){
+            $db = new Connection();
+            $query = "DELETE FROM `cartitemsvendors` WHERE `cartitemsvendors`.`cartitem_id`= $cartitem_id ";
+            $db->query($query);
+            if ($db->affected_rows >= 0) {
+                return TRUE;
+            }
+            return FALSE;
+        }
+
 
     }
+
+
+    /*
+    
+            public static function insertCartItemVendor($vendor_id, $product_id, $quantity){
+            
+            $db = new Connection();
+            $query = "INSERT INTO `cartitemsvendors`(`vendor_id`, `product_id`, `quantity`) VALUES ('".$vendor_id."', '".$product_id."', '".$quantity."')";
+            $db->query($query);
+            if ($db->affected_rows) {
+                return TRUE;
+            }
+            return FALSE;
+
+        }
+    
+    */
