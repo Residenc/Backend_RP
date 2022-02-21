f31c219299f19a26b8b8995af4963f2fbabb488d
Gabriel0206 <84149222+Gabriel0206@users.noreply.github.com>
2022-02-21 10:15:54 -0600

    Se arreglan comandos

--- api nodejs/index.js
+++ api nodejs/index.js
@@ -47,3 +47,48 @@ app.get('/', (req, res) =>{
          callBack(null, file.originalname)
      }
  });
+
+         const upload = multer({ storage: storage })
+
+ 
+ app.get("/upload", (req, res) => {
+    conexion.query('SELECT products.product_name,files.file_id,files.name, files.path as imagen FROM products INNER JOIN files ON products.product_id = files.id_producto where products.product_id = 98 and files.produc_id = 98;', (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post('/file', upload.array('files'), (req, res, next) => {
+    const files = req.files;
+    
+
+    if (!files) {
+        const error = new Error('No File')
+        error.httpStatusCode = 400;
+        return next(error)
+    }
+    
+        const sql = 'select product_id from products order by product_id desc limit 1;';
+     
+        conexion.query(sql,(error, results) => {
+            if (error) throw error;
+            console.log(error);
+            if (results.length > 0){
+                res.json(results)
+                const data = results[0].product_id;
+                for (let clave in files) {
+                    const file = files[clave];
+                    const nombreArchivo = /*uuidv4().concat*/(file.filename);
+                    const direccion = file.path;
+                    conexion.query('INSERT INTO files (name,path,product_id) values (? , ?, ?)', [nombreArchivo,direccion,data]);
+                  }
+            }else{
+                res.send('No hay resultados')
+            } 
+
+    })
+
+});
